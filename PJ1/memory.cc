c27b3d0f251a73a9db06656928db7b9703bc247a
edd3173 <63847412+edd3173@users.noreply.github.com>
2022-02-07 23:49:21 +0900

    Initial Commit

---
+++ PJ1/memory.cc
@@ -0,0 +1,79 @@
+﻿#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char* getPortNum(char* str) {
+	char* sArr[10] = { NULL, }; int i = 0;
+	char dftPort[3] = "80";
+	char* ptr = strtok(str, ":");
+	while (ptr != NULL) {
+		sArr[i] = ptr;
+		i++;
+		ptr = strtok(NULL, "/");
+	}
+	if (sArr[1] != NULL) {
+		printf("PortStr : %s\n", sArr[1]);
+		return sArr[1];
+	}
+	else
+	{
+		printf("Default port str\n");
+		return dftPort;
+	}
+}
+
+
+char* getHostName(char* str) {
+	char* sArr[10] = { NULL, }; int i = 0;
+	char* ptr = strtok(str, ":");
+	while (ptr != NULL) {
+		sArr[i] = ptr;
+		i++;
+		ptr = strtok(NULL, "/");
+	}
+	//printf("HostName : %s\n", sArr[0]);
+	return sArr[0];
+}
+
+char* getFileName(char* str) {
+
+}
+
+int main() {
+	char str[128];
+	fgets(str, strlen(str), stdin);
+	str[strlen(str) - 1] = '\0';
+	printf("your input : %s\n", str);
+	if (strncmp(str, "http://", 7) != 0) {
+		fprintf(stderr, "wrong format!\n");
+		exit(0);
+	}
+	char* buff = (char*)malloc(sizeof(char) * 128);
+	// http://www.sogang.ac.kr
+	for (int i = 7; i < strlen(str); i++)
+		buff[i - 7] = str[i];
+
+	// must put buff[16] = '\0'
+	// strlen(str) : 24, 
+	// 16=24-7-1
+
+	buff[strlen(str) - 7] = '\0';
+	
+	printf("After http:// -> [%s]\n", buff);
+
+	int portNum = 80;
+	char* portStr = getPortNum(buff);
+	char* hostname = getHostName(buff);
+	//char* filename = getFileName(buff);
+
+	if (strcmp(portStr, "80") != 0) {
+		// need to update portNum
+		portNum = atoi(portStr);
+	}
+
+	//printf("hostname : %s\nportNumber:%s\n", hostname, portStr);
+	printf("hostname : %s\nportNumber:%d\n", hostname, portNum);
+
+}
---
+++ PJ1/selectserver.cc
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define MAXDATASIZE 1000
+#define BACKLOG 10
+
+int main(int argc, char* argv[]){
+
+	int sockfd;
+	int new_fd;
+	struct addrinfo hints, *servinfo;
+	struct sockaddr_storage their_addr;
+	socklen_t sin_size;
+	char s[INET_ADDRSTRLEN];
+	int rv;
+
+	fd_set master;
+	fd_set read_fds;
+	int fdmax;
+
+	char buf[MAXDATASIZE];
+	int numbytes;
+
+	if(argc !=2){
+		printf("usage: server portnum\n");
+		exit(1);
+	}
+
+	memset(&hints,0,sizeof hints);
+	hints.ai_family=AF_UNSPEC;
+	hints.ai_socktype=SOCK_STREAM;
+	hints.ai_flags=AI_PASSIVE;
+
+	rv=getaddrinfo(NULL,argv[1],&hints,&servinfo);
+	sockfd=socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+	bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	freeaddrinfo(servinfo);
+
+	listen(sockfd, BACKLOG);
+	printf("server : waiting for connections on port %s...\n",argv[1]);
+
+	FD_ZERO(&master); // initialization
+	FD_SET(sockfd, &master);
+	fdmax=sockfd;
+
+	while(1){
+		read_fds=master; // readfds copy
+		if(select(fdmax+1,&read_fds,NULL,NULL,NULL)==-1){
+			perror("select");
+			exit(1);
+		}
+
+		for(int i=0;i<=fdmax;i++){
+			if(FD_ISSET(i,&read_fds)){
+				if(i==sockfd){
+					sin_size=sizeof their_addr;
+					//blocking
+					new_fd=accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);
+					if(new_fd==-1){
+						perror("accept");
+						exit(1);
+					}
+					else{
+						FD_SET(new_fd, &master);
+						if(new_fd>fdmax)
+							fdmax=new_fd;
+					}
+
+					inet_ntop(their_addr.ss_family, &((struct sockaddr_in*)&their_addr)->sin_addr,s,sizeof s);
+					printf("server: got connection from %s\n",s);
+				}
+				else{
+					if((numbytes=recv(i,buf,sizeof buf, 0))<=0){
+						if(numbytes==0)
+							printf("selectserver: socket %d hung up\n",i);
+						else
+							perror("recv");
+						close(i);
+						FD_CLR(i,&master);
+					}
+					else{
+						buf[numbytes]='\0';
+						printf("server received : %s\n",buf);
+
+						if(send(i,buf,strlen(buf),0)==-1)
+							perror("send");
+					}
+			}
+		}
+	}
+}
+	return 0;
+}
